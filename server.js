39b67a10174adefa7dc2168f8aa724d7f4d50574
Shimizu <shimizulazy@gmail.com>
2025-03-21 21:52:23 +0700

    fix path

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Phục vụ file tĩnh từ thư mục gốc
 app.use(express.static(path.join(__dirname)));
 
-// Redirect mọi request về index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
